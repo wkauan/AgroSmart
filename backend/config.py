92cb801b6dd27b5bee74286cd0340dc69604d47c
wkauan <kauanmbomfim5@gmail.com>
2024-06-16 20:45:39 -0300

    :sparkles: Feat: Adicionando configs do backend

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 cors = CORS(app, origins="*")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
-app.condig["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///agrosmart.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
