0e9fd301b9cc5a964c3774449a98cd3f301b2e29
wkauan <kauanmbomfim5@gmail.com>
2024-06-16 20:47:28 -0300

    :sparkles: Feat: Teste de rota

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,17 @@
+from flask import jsonify
+from config import app, db
+from models import Contact
 
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    users = User.query.all()
+    json_users = list(map(lambda x: x.to_json(), users))
+    return jsonify({
+        "users": json_users
+    })
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True, port=8000)
