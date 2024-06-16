11d28e60e70238384c8e9167daddc4d0273f022b
wkauan <kauanmbomfim5@gmail.com>
2024-06-16 20:46:02 -0300

    :sparkles: Feat: Adicionando models do bd

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,31 @@
 from config import db
 
-class Contat(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
-    first_name = db.Column(db.String(80), unique=False, nullable=False)
-    last_name = db.Column(db.String(80), unique=False, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    telefone = db.Column(db.String(11), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
 
     def to_json(self):
         return {
             "id": self.id,
-            "firstName": self.first_name,
-            "lastName": self.last_name,
-            "email": self.email,
+            "Name": self.name,
+            "Telefone": self.telefone,
+            "Email": self.email,
+            "Password": self.password,
+        }
+
+class Contact(db.Model):
+    idContato = db.Column(db.Integer, primary_key=True)
+    nameContato = db.Column(db.String(80), unique=False, nullable=False)
+    telefoneContato = db.Column(db.String(11), unique=True, nullable=False)
+    emailContato = db.Column(db.String(120), unique=True, nullable=False)
+
+    def to_json(self):
+        return {
+            "idContato": self.idContato,
+            "NameContato": self.nameContato,
+            "TelefoneContato": self.telefoneContato,
+            "EmailContato": self.emailContato,
         }
