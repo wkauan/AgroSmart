74b0d9fece02768178871325b08e9c81fba0e0fb
wkauan <kauanmbomfim5@gmail.com>
2024-06-19 01:21:34 -0300

    :sparkles: Feat: Melhorando mensagens do sistema com o Toast

--- frontend/src/pages/cadastro/Cadastro.jsx
+++ frontend/src/pages/cadastro/Cadastro.jsx
@@ -16,6 +16,34 @@ const Cadastro = () => {
 
     });
 
+    const notifyError = (message) => {
+        toast.error(message, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+    };
+
+    const notifySuccess = (message) => {
+        toast.success(message, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+    }
+
     const fetchCadastro = async (e) => {
         try {
             e.preventDefault();
