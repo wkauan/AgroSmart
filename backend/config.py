cc88b57fd20b569461e95a63e6cdd831c4b0d310
wkauan <kauanmbomfim5@gmail.com>
2024-06-19 01:19:01 -0300

    :recycle: Refact: Refatorando codigo

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-cors = CORS(app, origins="*")
+cors = CORS(app, origins='http://localhost:5173')
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///agrosmart.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     name = db.Column(db.String(80), unique=False, nullable=False)
     telefone = db.Column(db.String(11), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
 
     def to_json(self):
         return {
