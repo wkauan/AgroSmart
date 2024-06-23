5c6b5a0bb9340493639da5d3106cf515efdf838f
wkauan <kauanmbomfim5@gmail.com>
2024-06-23 11:17:15 -0300

    Feat: Melhorando o Lazy Loading

--- frontend/src/components/fallback/Loading.jsx
+++ frontend/src/components/fallback/Loading.jsx
@@ -1,9 +1,14 @@
+import Loader from '/static/loader/loader.gif';
+
 export default function Loading() {
     return (
-        <div className="flex justify-center h-screen mt-60 md:mt-44">
-            <p className="text-gray-700 text-xl italic">
-                Veja tudo o que a AgroSmart pode te oferecer!
-            </p>
+        <div className="flex justify-center items-center h-screen">
+            <div className="text-center">
+                <img src={Loader} alt="Loading..." className="w-16 h-16 mx-auto" />
+                <p className="mt-4 text-gray-700 text-xl italic font-semibold">
+                    Descubra tudo o que a AgroSmart pode fazer por você!
+                </p>
+            </div>
         </div>
     );
 }
